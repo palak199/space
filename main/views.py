ac8859c0507e3e38215d169ab421a0ae7289e1aa
palak199 <p.thapar99@gmail.com>
2020-10-04 22:40:06 +0530

    changes

--- main/views.py
+++ main/views.py
@@ -12,30 +12,29 @@ import datetime
 from django.contrib.auth.models import User
 from PIL import Image, ImageDraw, ImageFont
 from django.core import serializers
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def index(request):
-    user = request.user
-    carbon = Carbon_fp.objects.filter(user=user)
-    carbon_data = []
-    for i in carbon:
-        new = {}
-        new['y'] = i.carbon
-        carbon_data.append(new)
-        print(i.carbon)
-    
-    print(carbon_data)
-    carbon = dumps({'c_data' : carbon_data})
-    context = {
-            
-            'carbon':carbon,
-    }
-    print(carbon)
-    return render(request, 'index.html',context)
 
-
-
-    if not User.is_anonymous:
+    x=request.user.is_authenticated
+    if request.user.is_authenticated:
+        user = request.user
+        carbon = Carbon_fp.objects.filter(user=user)
+        carbon_data = []
+        for i in carbon:
+            new = {}
+            new['y'] = i.carbon
+            carbon_data.append(new)
+            print(i.carbon)
+        
+        print(carbon_data)
+        carbon = dumps({'c_data' : carbon_data})
+        context = {
+                
+                'carbon':carbon,
+        }
         # user = request.user.first_name
         # img = Image.open('main/static/images/certificate.jpg')
         # d1 = ImageDraw.Draw(img)
@@ -44,34 +43,33 @@ def index(request):
         # img.save("main/static/images/image_text.jpg")
         
         try:
-            score = 0
             
-            source = requests.get('http://124.253.142.66').text
-            soup=BeautifulSoup(source,'lxml')
-            match=soup.p.text
+            # source = requests.get('http://27.255.226.228').text
+            # soup=BeautifulSoup(source,'lxml')
+            # match=soup.p.text
            
            
-            context = {
+            # context = {
                 
-                'data':match,
-                # 'dataJSON':dataJSON
-            }
+            #     'data':match,
+            #     'carbon':carbon,
+            # }
         
-            user=request.user
-            if user:
-                today = datetime.datetime.now().date()
-                x=Water_level.objects.filter(user=request.user).first()
+            # user=request.user
+            # if user:
+            #     today = datetime.datetime.now().date()
+            #     x=Water_level.objects.filter(user=request.user).first()
 
-                if x :
-                    y=x.time.date()
-                    if y==today:
-                        return render(request,'index.html',context)
-                    else:
-                        new=Water_level.objects.create(user=request.user)
-                        new.save()
-                else:
-                    new=Water_level.objects.create(user=request.user)
-                    new.save()
+            #     if x :
+            #         y=x.time.date()
+            #         if y==today:
+            #             return render(request,'index.html',context)
+            #         else:
+            #             new=Water_level.objects.create(user=request.user)
+            #             new.save()
+            #     else:
+            #         new=Water_level.objects.create(user=request.user)
+            #         new.save()
                 
                 return render(request, 'index.html',context)
         except:
@@ -93,14 +91,6 @@ def index(request):
             else:
                 return render(request, 'index.html',{'carbon':carbon})
     else : 
-        # user =request.user
-        # carbon = Carbon_fp.objects.filter(user=user)
-        # carbon = serializers.serialize('json', carbon)
-        
-        # carbon = dumps(carbon)
-        # print(dataJSON)
-
-        # return render(request, 'index.html',{'carbon':carbon})
         return render(request, 'index.html')
 
 
@@ -112,5 +102,3 @@ def survey(request):
     return render(request,'survey-individual.html')   
 def leaderboard(request):
     return render(request,'leaderboard.html')
-
-
